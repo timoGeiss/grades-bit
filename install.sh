2a0a757d32530defd1e944b62e5d65357b97f795
timoGeiss <134839849+timoGeiss@users.noreply.github.com>
2025-02-25 11:14:18 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Define variables
+BRANCH_NAME="revert"
+FOLDER_NAME="grades-bit"
+REPO_URL="https://github.com/timoGeiss/grades-bit.git"
+
+# Check if the folder exists and delete it if present
+if [ -d "$FOLDER_NAME" ]; then
+    echo "Folder $FOLDER_NAME exists. Deleting..."
+    rm -rf "$FOLDER_NAME"
+else
+    echo "Folder $FOLDER_NAME does not exist. Proceeding..."
+fi
+
+# Clone the repository
+git clone $REPO_URL
+
+cd $FOLDER_NAME
+
+git checkout $BRANCH_NAME && git pull origin $BRANCH_NAME
+
+# Delete .git folder before running npm install
+rm -rf .git
+
+# Run npm install
+npm install
