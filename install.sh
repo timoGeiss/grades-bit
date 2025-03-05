0578f275cac770a416a913a3bded8b5d6aa204fe
Mica Mutti <mica.mutti@bit.admin.ch>
2025-03-05 10:05:22 +0100

    Added lines to load nvm without reopen terminal

--- install.sh
+++ install.sh
@@ -33,6 +33,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 sleep 1
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm
+
 source ~/.bashrc
 
 echo "Installing node 18"
