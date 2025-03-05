e5317ad3b575107260be75b7b8c9de45ee184bf1
CryptoPhantom99 <mica.mutti@bit.admin.ch>
2025-03-05 10:11:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,13 +31,11 @@ chmod 777 ./DumpCodebase.sh
 echo "Curling nvm..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-sleep 1
+sleep 4
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm
-
-source ~/.bashrc
 
 echo "Installing node 18"
 nvm install 18
