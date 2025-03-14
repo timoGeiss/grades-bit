3623a039c73c5b3b1e01492660c591fbbde59ac4
bbuelu <lukas.buehlmann@bit.admin.ch>
2025-03-14 13:43:52 +0100

    Aufgabe 11: Code angepasst (nun so wie in Anleitung)

--- application/grades-bit/components/Listen/NotenListeItem.js
+++ application/grades-bit/components/Listen/NotenListeItem.js
@@ -4,32 +4,14 @@ import {useState} from "react";
 
 export default function NotenListeItem({note}) {
     function navigiereZuDetailAnsicht() {
-        router.push(`note/${note.id}`)
-    }
-
-    function erhalteFarbeNachNotenWert() {
-        if (note.wert >= 5.5) {
-            return "green"
-        }
-        if (note.wert >= 5 && note.wert < 5.5) {
-            return "lightgreen"
-        }
-        if (note.wert >= 4.5 && note.wert < 5) {
-            return "yellow"
-        }
-        if (note.wert >= 4 && note.wert < 4.5) {
-            return "orange"
-        }
-        if (note.wert < 4) {
-            return "red"
-        }
+        // Navigiere hier zur Noten Detailansicht
+        // Pfad: note/id
     }
 
     return (
         <TouchableOpacity onPress={navigiereZuDetailAnsicht} style={styles.container}>
             <Text style={[styles.text, styles.titel]} numberOfLines={1} ellipsizeMode={"tail"}>{note.titel}</Text>
             <Text style={[styles.text, styles.wert]}>{note.wert}</Text>
-            <View style={[styles.balken, {backgroundColor: erhalteFarbeNachNotenWert()}]}><Text></Text></View>
         </TouchableOpacity>
     )
 }
