c9c70759f7a7e3ed7698df21e13c7e480a802f38
Geissbühler Timo BIT <timo.geissbuehler@bit.admin.ch>
2025-04-28 09:50:32 +0200

    content of the commit before

--- application/grades-bit/app/note/create.js
+++ application/grades-bit/app/note/create.js
@@ -1,14 +1,14 @@
-import {View, Text, StyleSheet, React} from "react-native";
+import { View, Text, StyleSheet, React } from "react-native";
 import Textfeld from "../../components/Eingaben/Textfeld";
-import {useState} from "react";
+import { useState } from "react";
 import Zahlenfeld from "../../components/Eingaben/Zahlenfeld";
 import Knopf from "../../components/Eingaben/Knopf";
-import {insertIntoFach, insertIntoNote} from "../../database";
-import {router, useLocalSearchParams, useRouter} from "expo-router";
-import {StatusBar} from "expo-status-bar";
+import { insertIntoFach, insertIntoNote } from "../../database";
+import { router, useLocalSearchParams, useRouter } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function NoteErstellen() {
-    const {id} = useLocalSearchParams()
+    const { id } = useLocalSearchParams()
 
     const [titel, titelSetzen] = useState("");
     const [note, noteSetzen] = useState(0);
@@ -16,29 +16,35 @@ export default function NoteErstellen() {
     const [error, errorSetzen] = useState(null)
 
     async function formularBestätigt() {
-        // TODO: Baue hier die Validierung des Titels, der Note und der Gewichtung ein.
-        // Insgesamt 5-mal If
+        // TODO: Baue hier die Validierung, des Titels, der Note und der Gewichtung ein.
+        // Setzte die Bedingungen und das ErrorSetzten bei den übrigen Ifs
+        
+        // Lösche das /* unterhalb dieses Kommentares
+        /*
 
-        const überprüfteNote = Number(note)
-        if (isNaN(überprüfteNote)) {
-            errorSetzen("Die Note muss eine Zahl sein (Nur . erlaubt kein ,)")
-            return
+        if (titel.length < 2) {
+            errorSetzen("Titel ist zu kurz")
+        } else if (Titel Länge maximal 20) {
+
+        } else if (Note mindestens 1) {
+
+        } else if (Note maximum 6) {
+
+        } else if (Gewichtung mindestens 1) {
+
+        } else {
+
+            await insertIntoNote(id, titel, note, gewichtung);
+
+            // TODO: Navigiere hier zurück (Das hast du schon einmal auf der Facherstellungsseite gemacht)
         }
 
-        const überprüfteGewichtung = Number(gewichtung)
-        if (isNaN(überprüfteGewichtung)) {
-            errorSetzen("Die Gewichtung muss eine Zahl sein (Nur . erlaubt kein ,)")
-            return
-        }
-
-        await insertIntoNote(id, titel, überprüfteNote, überprüfteGewichtung);
-
-        // TODO: Navigiere hier zurück (Das hast du schon einmal auf der Facherstellungsseite gemacht)
+        /**/
     }
 
     return (
         <View style={styles.container}>
-            <StatusBar/>
+            <StatusBar />
             <Text style={styles.bigText}>Was für eine Note möchtests du erstellen?</Text>
             <Textfeld
                 titel={"Titel der Prüfung"}
@@ -56,7 +62,7 @@ export default function NoteErstellen() {
                       Überlege selbst, wie du dies einbauen musst.*/}
 
             {error ? <Text style={styles.error}>{error}</Text> : null}
-            <Knopf beimKlicken={formularBestätigt} text={"Bestätigen"}/>
+            <Knopf beimKlicken={formularBestätigt} text={"Bestätigen"} />
         </View>
     )
 }
