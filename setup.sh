9177d8326b545d33e768760b4a3a61e61aa9f42d
Mica Mutti <mica.mutti@bit.admin.ch>
2026-02-13 16:07:07 +0100

    Added curl install to script setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,19 @@
 
 USERNAME="ubuntu"
 
+# ---- ensure curl is installed ----
+if ! command -v curl >/dev/null 2>&1; then
+  echo "Installing curl..."
+  apt update
+  apt install -y curl
+fi
+
 ## create alias for setup if not exists
 if ! grep -qE "^alias\s+setup=" /home/$USERNAME/.bashrc; then
   echo -e "alias setup=\"gsettings set org.gnome.desktop.input-sources sources \\\"[('xkb','ch')]\\\" && sudo /usr/local/bin/setup.sh\"" | tee -a /home/$USERNAME/.bashrc
 fi
 
+#cloning repo
 SETUP_C="curl -L https://raw.githubusercontent.com/timoGeiss/grades-bit/main/install.sh | bash"
 sudo -u $USERNAME sh -c "cd /home/$USERNAME/ && $SETUP_C"
 
